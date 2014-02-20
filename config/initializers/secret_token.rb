21a88db8c9d951d56647c70cca8372373e54699f
Breno da Mata <bjrdamata@gmail.com>
2014-02-20 12:20:56 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Breno::Application.config.secret_key_base = 'e902f758b6cf89197af241a27d56ce4001f9f4d8b058f5c0b3fcc762c90160ce93d1a22821fb6b892059ea8da8ab2e88b0b32d32f489e93e8340024958ec4e73'
